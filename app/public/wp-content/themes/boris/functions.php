f62f7ae6d8994d4619709bbddbbab21b2d72dc59
bgwebdes <esleisbo@yahoo.fr>
2023-01-11 11:15:43 +0100

    mise en place du responsive

--- app/public/wp-content/themes/boris/functions.php
+++ app/public/wp-content/themes/boris/functions.php
@@ -6,3 +6,15 @@ function theme_enqueue_styles()
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/theme.css', array(), filemtime(get_stylesheet_directory() . '/theme.css'));
 }
+function logged_wp_nav_menu_args( $args = '' ) {
+    if( is_user_logged_in() ) {
+    // Logged in menu to display
+    $args['menu'] = 3;
+     
+    } else {
+    // Non-logged-in menu to display
+    $args['menu'] = 2;
+    }
+    return $args;
+    }
+    add_filter( 'wp_nav_menu_args', 'logged_wp_nav_menu_args' );
