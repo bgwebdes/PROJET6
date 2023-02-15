22120644156026842e02fccfe7a2f2f9d9fb0d93
bgwebdes <esleisbo@yahoo.fr>
2023-02-15 12:34:27 +0100

    mise à jour taille policeh1

--- app/public/wp-content/themes/boris/functions.php
+++ app/public/wp-content/themes/boris/functions.php
@@ -6,6 +6,9 @@ function theme_enqueue_styles()
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/theme.css', array(), filemtime(get_stylesheet_directory() . '/theme.css'));
 }
+
+
+//Menu de navigation utilisateur connecté ou non
 function logged_wp_nav_menu_args( $args = '' ) {
     if( is_user_logged_in() ) {
     // Utilisateur connecté : affiche le menu Admin
